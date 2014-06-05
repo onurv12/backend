eaf5ca1b115552781cafa94700364c676ac1e1ee
Tobias Mücksch <tobias@muecks.ch>
2014-06-05 23:38:03 +0200

    Refactor backend structure
    by storing route logic into controller

---
+++ controller/User.class.php
@@ -0,0 +1,69 @@
+<?php
+
+abstract class UserController {
+
+	public static function login () {
+		$DB = Flight::DB();
+		$userManager = Flight::userManager();
+
+		$headers = apache_request_headers();
+		$request = Flight::request();
+
+		if (isset($headers["username"], $headers["password"])) {
+			try {
+				$userdata = $userManager->login($headers["username"], $headers["password"]);
+
+				if ($userdata) {
+					echo json_encode(true);
+				} else {
+					echo json_encode(false);
+				}
+			} catch (Exception $e) {
+				Flight::halt(401, "401 - User suspended");
+			}
+		} else {
+			Flight::halt(400, "400 - Bad Request");
+		}
+	}
+
+	public static function logout () {
+		$userManager = Flight::userManager();
+
+		$userManager->logout();
+		echo "";
+	}
+
+	public static function registration () {
+		$userManager = Flight::userManager();
+
+		if (!$userManager->getLoginState()) {
+			$request = Flight::request();
+			$json = json_decode($request->body, true);
+
+			if (!isset($json["GravatarEmail"])) {
+				$json["GravatarEmail"] = null;
+			}
+
+			
+			if (isset($json["Name"], $json["Fullname"], $json["Password"], $json["Email"])) {
+				if (!(validation::minLength($json["Name"], 3) && validation::minLength($json["Fullname"], 3) && validation::minLength($json["Password"], 6) && validation::email($json["Email"]) && ($json["GravatarEmail"] == null || validation::email($json["GravatarEmail"])))) {
+					Flight::halt(400, "Bad request.");
+				}
+
+				try {
+					$userManager->createUser($json["Name"], $json["Fullname"], $json["Password"], $json["Email"], $json["GravatarEmail"]);
+				} catch (Exception $e) {
+					Flight::halt(409, "Conflict. User already exists.");
+				}
+
+				echo "true";
+			} else {
+				Flight::halt(400, "Missing information");
+			}
+		} else {
+			Flight::halt(412, "412 - You may not be logged in while registering a new user.");
+		}
+	}
+}
+
+?>
--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require 'vendor/mikecao/flight/flight/Flight.php';
 require '../dbWrapper/dbWrapper.class.php';
 require '../userManagement/userManager.class.php';
 require 'validation.class.php';
+require 'controller/User.class.php';
 
 $dbSettings = Array();
 // DBName
@@ -34,74 +35,16 @@ Flight::route('/', function () {
 });
 
 Flight::route('GET /login', function () {
-
-	$DB = Flight::DB();
-	$userManager = Flight::userManager();
-
-	$headers = apache_request_headers();
-	$request = Flight::request();
-
-	if (isset($headers["username"], $headers["password"])) {
-		try {
-			$userdata = $userManager->login($headers["username"], $headers["password"]);
-
-			if ($userdata) {
-				echo json_encode(true);
-			} else {
-				echo json_encode(false);
-			}
-		} catch (Exception $e) {
-			Flight::halt(401, "401 - User suspended");
-		}
-	} else {
-		Flight::halt(400, "400 - Bad Request");
-	}
-
+	UserController::login();
 });
 
 Flight::route('GET /logout', function () {
-
-	$userManager = Flight::userManager();
-
-	$userManager->logout();
-	echo "";
+	UserController::logout();
 });
 
 // Registration
 Flight::route('POST /user', function () {
-
-	$userManager = Flight::userManager();
-
-	if (!$userManager->getLoginState()) {
-		$request = Flight::request();
-		$json = json_decode($request->body, true);
-
-		if (!isset($json["GravatarEmail"])) {
-			$json["GravatarEmail"] = null;
-		}
-
-		
-		if (isset($json["Name"], $json["Fullname"], $json["Password"], $json["Email"])) {
-			var_dump(validation::minLength($json["Name"], 3), validation::minLength($json["Fullname"], 3), validation::minLength($json["Password"], 6), validation::email($json["Email"]), $json["GravatarEmail"] == null, validation::email($json["GravatarEmail"]));
-
-			if (!(validation::minLength($json["Name"], 3) && validation::minLength($json["Fullname"], 3) && validation::minLength($json["Password"], 6) && validation::email($json["Email"]) && ($json["GravatarEmail"] == null || validation::email($json["GravatarEmail"])))) {
-				Flight::halt(400, "Bad request.");
-			}
-
-			try {
-				$userManager->createUser($json["Name"], $json["Fullname"], $json["Password"], $json["Email"], $json["GravatarEmail"]);
-			} catch (Exception $e) {
-				Flight::halt(409, "Conflict. User already exists.");
-			}
-
-			// TODO: Send a mail here
-			echo "true";
-		} else {
-			Flight::halt(400, "Missing information");
-		}
-	} else {
-		Flight::halt(412, "412 - You may not be logged in while registering a new user.");
-	}
+	UserController::registration();
 });
 
 //////////////////////////////////////////////////////
