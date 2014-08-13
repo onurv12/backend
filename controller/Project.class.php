95bc839d73ecefa1c0694a564481b2714e82bcbe
Lukas Appelhans <l.appelhans@gmx.de>
2014-08-13 23:28:33 +0200

    Add createProject route to flight and implement it

--- controller/Project.class.php
+++ controller/Project.class.php
@@ -1,6 +1,47 @@
 <?php
 
 abstract class ProjectController {
+
+	public static function createProject() {
+		$DB = Flight::DB();
+		$productionManager = Flight::ProductionManager();
+		$userManager = Flight::UserManager();
+		$request = Flight::request();
+		
+		if (!$userManager->getSession())
+			Flight::halt(403, "Please login!");
+		
+		if (isset($request->data->Name) && isset($request->data->Director) && $userManager->getUserID($request->data->Director)) {
+			if (!$userManager->checkAdmin() && $request->data->Director != $userManager->getSession()["Name"]) {
+				Flight::halt(403, "You are not logged in as admin nor are you the director. For that reason you cannot create the project.");
+			}
+			$projectId = $productionManager->createProject($request->data->Name, $request->data->Description, $userManager->getUserID($request->data->Director));
+			if ($projectId) {
+				$supervisors = $request->data->supervisors;
+				$artists = $request->data->artists;
+				$count = count($supervisors);
+				for ($i = 0; $i < $count; $i++) {
+					$userId = $userManager->getUserID($supervisors[$i]["Name"]);
+					if (!$userId || $supervisors[$i]["Name"] == $request->data->Director)
+						continue;
+					$productionManager->addUser2Project($userId, $projectId, "Supervisor");
+				}
+				$count = count($artists);
+				for ($i = 0; $i < $count; $i++) {
+					$userId = $userManager->getUserID($artists[$i]["Name"]);
+					if (!$userId || in_array($artists[$i]["Name"], $supervisors) || $artists[$i]["Name"] == $request->data->Director)
+						continue;
+					$productionManager->addUser2Project($userId, $projectId, "Artist");
+				}
+				
+				Flight::json(true);
+			} else {
+				Flight::halt(409, "Project name already exists!");
+			}
+		} else {
+			Flight::halt(403, "Please specify at least name and director");
+		}
+	}
 	
 	public static function getAllProjects(){
 		$productionManager = Flight::ProductionManager();
