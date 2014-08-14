43876a24a38af11a4f10a1ceb50a362c3d4696fa
Lucas <lucas@paperdreamer.org>
2014-08-14 16:34:14 +0200

    Implement functionality triggered by routes for getting projects and canvases

--- controller/Project.class.php
+++ controller/Project.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once "../userManagement/CanvasManager.php";
 
 abstract class ProjectController {
 	
@@ -20,6 +21,49 @@ abstract class ProjectController {
 			$belongedProjects = Array();
 		}
 		Flight::json($belongedProjects);
+	}
+
+	// Gets a certain project respecting the ID
+	public static function get($ID) {
+		// TODO: Make sure the user is allowed to access this project...!
+		$DB = Flight::DB();
+		$productionManager = Flight::ProductionManager();
+		$canvasManager = new CanvasManager($DB);
+
+		$projectInfo = $productionManager->getProject($ID);
+
+		$panelsTmp = $canvasManager->getPanels($ID);
+		$panels = array();
+
+		if (!count($panelsTmp) || !$projectInfo) {
+			Flight::halt(404, "This project could not be found.");
+		}
+
+		foreach ($panelsTmp as $panel) {
+			$panel["Assets"] = $canvasManager->getAssets($panel["ID"]);
+
+			$panels[] =$panel;
+		}
+
+		$project = $projectInfo;
+		$project["Panels"] = $panels;
+
+		Flight::json($project);
+	}
+
+
+	public static function getCanvas ($ProjectID, $CanvasID) {
+		// TODO: Make sure the user is allowed to access this project...!
+		$DB = Flight::DB();
+		$canvasManager = new CanvasManager($DB);
+
+		$canvas = $canvasManager->getCanvas($ProjectID, $CanvasID);
+
+		if ($canvas) {
+			$canvas["Assets"] = $canvasManager->getAssets($CanvasID);
+		}
+
+		Flight::json($canvas);
 	}
 }
 
